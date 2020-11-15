3b3fad5a7d8f1d2aabae7360773e1ed680f66564
User <ngoc1997307@gmail.com>
2020-11-15 15:16:08 +0700

    Chart Horizonal

--- src/components/PlayerLog.js
+++ src/components/PlayerLog.js
@@ -1,7 +1,9 @@
 import React, {Component, useState} from 'react';
-import {StyleSheet, View, Animated} from 'react-native';
+import {StyleSheet, View, Animated, TouchableOpacity, Dimensions} from 'react-native';
+import Texts from 'react-native/Libraries/Text/Text'
 import {Svg, G, Line, Rect, Text} from 'react-native-svg';
 import * as d3 from 'd3';
+import { back } from 'react-native/Libraries/Animated/src/Easing';
 const data = [
   {label: 'Jan', value: 500, color: '#F55443'},
   {label: 'Feb', value: 312, color: '#FCBD24'},
@@ -17,16 +19,31 @@ const data = [
   {label: 'Dec', value: 643, color: '#4D98E4'},
 ];
 
+const datas = [
+  {label: 'Jan', value: 300, color: '#F55443'},
+  {label: 'Feb', value: 200, color: '#FCBD24'},
+  {label: 'Mar', value: 100, color: '#59838B'},
+  {label: 'Apr', value: 450, color: '#4D98E4'},
+  {label: 'May', value: 89, color: '#418E50'},
+  {label: 'Jun', value: 434, color: '#7B7FEC'},
+  {label: 'Jul', value: 650, color: '#3ABAA4'},
+  {label: 'Aug', value: 290, color: '#F55443'},
+  {label: 'Sep', value: 321, color: '#FCBD24'},
+  {label: 'Oct', value: 186, color: '#59838B'},
+  {label: 'Nov', value: 190, color: '#F55443'},
+  {label: 'Dec', value: 467, color: '#4D98E4'},
+];
+
 const GRAPH_MARGIN = 20;
-const GRAPH_BAR_WIDTH = 5;
+const GRAPH_BAR_WIDTH = 8;
 const colors = {
   axis: '#E4E4E4',
   bars: '#15AD13',
 };
 const PlayerLog = () => {
   // Dimensions
-  const SVGHeight = 150;
-  const SVGWidth = 300;
+  const SVGHeight = 350;
+  const SVGWidth = 400;
   const graphHeight = SVGHeight - 2 * GRAPH_MARGIN;
   const graphWidth = SVGWidth - 2 * GRAPH_MARGIN;
   const round = 100;
@@ -46,12 +63,27 @@ const PlayerLog = () => {
   // top axis and middle axis
   const middleValue = topValue / 2;
 
+  const [chart, setChart] = useState(data);
+
+  const next = () => {
+    setChart(data);
+  }
+
+  const back = () => {
+    setChart(datas);
+  }
+
   return (
     <View style={styles.container}>
       <Svg width={SVGWidth} height={SVGHeight}>
-        <G y={graphHeight + GRAPH_MARGIN}>
+        <G
+        x={10}
+        //  y={graphHeight + GRAPH_MARGIN}
+         >
+
+ 
           {/* Top value label */}
-          <Text
+          {/* <Text
             x={graphWidth}
             textAnchor="end"
             y={y(topValue) * -1 - 5}
@@ -59,10 +91,10 @@ const PlayerLog = () => {
             fill="black"
             fillOpacity={0.4}>
             {topValue + ' ' + unit}
-          </Text>
+          </Text> */}
 
           {/* top axis */}
-          <Line
+          {/* <Line
             x1="0"
             y1={y(topValue) * -1}
             x2={graphWidth}
@@ -70,10 +102,10 @@ const PlayerLog = () => {
             stroke={colors.axis}
             strokeDasharray={[3, 3]}
             strokeWidth="0.5"
-          />
+          /> */}
 
           {/* middle axis */}
-          <Line
+          {/* <Line
             x1="0"
             y1={y(middleValue) * -1}
             x2={graphWidth}
@@ -81,44 +113,66 @@ const PlayerLog = () => {
             stroke={colors.axis}
             strokeDasharray={[3, 3]}
             strokeWidth="0.5"
-          />
+          /> */}
 
           {/* bottom axis */}
-          <Line
+          {/* <Line
             x1="0"
             y1="2"
             x2={graphWidth}
             y2="2"
             stroke={colors.axis}
             strokeWidth="0.5"
-          />
+          /> */}
 
           {/* bars */}
-          {data.map((item) => (
+          {chart.map((item) => (
             <Rect
               key={'bar' + item.label}
-              x={x(item.label) - GRAPH_BAR_WIDTH / 2}
-              y={y(item.value) * -1}
+              // x={x(item.label) - GRAPH_BAR_WIDTH / 2}
+              // y={y(item.value) * -1}
+              x={20}
+              y={x(item.label) - GRAPH_BAR_WIDTH / 2}
               rx={2.5}
-              width={GRAPH_BAR_WIDTH}
-              height={y(item.value)}
+              width={y(item.value)}
+              height={GRAPH_BAR_WIDTH}
               fill={item.color}
             />
           ))}
 
           {/* labels */}
-          {data.map((item) => (
+          {chart.map((item) => (
             <Text
+              fill="#000"
               key={'label' + item.label}
               fontSize="8"
-              x={x(item.label)}
-              y="10"
-              textAnchor="middle">
+              x={20}
+              y={x(item.label) - 8}
+              textAnchor='start'>
+              {item.label}
+            </Text>
+          ))}
+           {chart.map((item) => (
+            <Text
+              fill="#000"
+              key={'label' + item.label}
+              fontSize="8"
+              x={y(item.value) + 30}
+              y={x(item.label) +3}
+              textAnchor='start'>
               {item.label}
             </Text>
           ))}
         </G>
       </Svg>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TouchableOpacity style={{margin: 20}} onPress={() =>next()}>
+            <Texts>Next</Texts>
+        </TouchableOpacity>
+        <TouchableOpacity style={{margin: 20}} onPress={() =>back()}>
+            <Texts>Back</Texts>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
